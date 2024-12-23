c1eb956f50e8d78bbdab68b93f7dfe8a7c7dd301
Melody Yu <yu.melo@northeastern.edu>
2024-12-22 23:22:11 -0500

    disabled linter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during builds
+  },
 };
 
 export default nextConfig;
